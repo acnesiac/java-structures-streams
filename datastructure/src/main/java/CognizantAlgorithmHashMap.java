d57007b05058627339a4001cf95ed8bcf6389379
Adolfo <adolfo.rivas@Lennar.com>
2024-01-22 16:52:59 +0100

    cognizant algorithm main

--- datastructure/src/main/java/CognizantAlgorithmHashMap.java
+++ datastructure/src/main/java/CognizantAlgorithmHashMap.java
@@ -1,5 +1,19 @@
 public class CognizantAlgorithmHashMap {
     public static void main(String[] args) {
 
+        //https://discuss.codechef.com/t/backtracking-the-power-sum-hackerranks-problem-recursion/17667/2
+//        Recursive Solution: sum(X, N, 1) gives the answer.
+//
+//        static int sum(int X, int N, int num){
+//
+//            int value = X - (int)Math.pow(num, N);
+//
+//            if(value < 0)return 0;
+//
+//            if(value == 0)return 1;
+//
+//            return sum(X, N, num+1) + sum(value,N,num+1);
+
+//        }
     }
 }
