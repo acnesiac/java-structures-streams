7415f2914114b2ba08de696f0e6fbe135602693d
Adolfo <adolfo.rivas@Lennar.com>
2024-01-22 16:42:06 +0100

    Java 21 And unosquare test

--- datastructure/src/main/java/codility/PermMissingElement.java
+++ datastructure/src/main/java/codility/PermMissingElement.java
@@ -2,4 +2,12 @@ package codility;
 
 public class PermMissingElement {
 
+    public static void main(String[] args) {
+    new PermMissingElement().func();
+    }
+
+    private void func() {
+
+    }
+
 }
