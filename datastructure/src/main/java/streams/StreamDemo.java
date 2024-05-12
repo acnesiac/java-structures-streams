6e9f2fdb75d8f35fb5dbf598f98f68e87e2eae49
acnesiac <xenbyl@gmail.com>
2024-05-12 06:00:44 -0600

    'newclass'

---
+++ datastructure/src/main/java/streams/StreamDemo.java
@@ -0,0 +1,14 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        List<Integer> collect = list.stream().map(n -> n + 1).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+    }
+
+}
--- javasynchro/src/EvenOddListSync.java
+++ javasynchro/src/EvenOddListSync.java
@@ -1,4 +1,5 @@
 
+
 public class EvenOddListSync {
 
     public static void main(String... args) {
