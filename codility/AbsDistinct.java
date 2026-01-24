b8190f7208a674d9d403dd4a35c50debee8f24d2
acnesiac <xenbyl@gmail.com>
2026-01-24 12:27:29 -0600

    distinct

--- codility/AbsDistinct.java
+++ codility/AbsDistinct.java
@@ -1,12 +1,18 @@
-package codility;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AbsDistinct {
 	public static void main(String[] args) {
-		System.out.println( AbsDistinct.solution(new int[]{ 1, 2, 3, 4, 5, -4 })
-		);
+		int[] a = {1, 2, 3, 4, 5, -4};
+		//System.out.println( AbsDistinct.solution(a)
+		//);
+
+		List<Integer> list = Arrays.asList(1, 8,1, 2, 3, 4, 5, -4, -4);
+		list.stream().distinct().forEach(System.out::println);
+
 	}
 	static int solution(int [] A){
 		Set set =  new HashSet();
