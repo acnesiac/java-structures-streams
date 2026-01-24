c425d44b4f33606e0f196a2917e66075c52c1cdd
acnesiac <xenbyl@gmail.com>
2026-01-23 23:23:25 -0600

    oddocurrencees in array

--- codility/OddOccurrencesInArray.java
+++ codility/OddOccurrencesInArray.java
@@ -1,41 +1,21 @@
-package codility;
+
 
 public class OddOccurrencesInArray {
-
-
-    public static void main(String[] args){
-        Printer p = new Printer();
-
-        ThreadP one = new ThreadP(p);
-        ThreadP two = new ThreadP(p);
-        new Thread(one);
-        new Thread(two);
+    public static void main(String[] args) {
+        occurrencesInArray(new int[]{1,1,3,9,9});
     }
 
-}
-
-class ThreadP implements Runnable{
-
-    Printer p = null;
-
-
-    ThreadP( Printer p){
-        this.p = p;
-    }
-
-    @Override
-    public void run() {
-
-    }
-}
-
-class Printer {
-
-    public synchronized void even(){
-
-    }
-
-    public synchronized  void odd(){
-
+    public static void occurrencesInArray(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < A.length; j++) {
+                if (A[i] == A[j]) {
+                    ++counter;
+                }
+            }
+            if (counter % 2 != 0 ) {
+                System.out.println(A[i]);
+            }
+        }
     }
 }
